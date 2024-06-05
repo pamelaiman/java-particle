16707716e50b674586640ab61ce5fa863bb32034
Pamela Iman <pamelariman@gmail.com>
2024-06-05 16:29:04 +0100

    file formatted

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ import processing.core.PApplet;
 
 public class Main extends PApplet {
     Spark[] sparks;
+
     public static void main(String[] args) {
         PApplet.main(new String[]{"Main"});
     }
+
     @Override
     public void settings() {
         size(800, 600);
@@ -19,12 +21,12 @@ public class Main extends PApplet {
 
     @Override
     public void draw() {
-    background(100);
-        for (Spark s : sparks){
+        background(100);
+        for (Spark s : sparks) {
             s.display();
         }
 
-        for (Spark s : sparks){
+        for (Spark s : sparks) {
             s.update();
         }
     }
